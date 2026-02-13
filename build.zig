e161ece09ca7ac1aa1c541abefc52a200ec2b653
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-02-13 20:45:16 +0000

    Add googletest testing

--- build.zig
+++ build.zig
@@ -4,6 +4,8 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const googletest_dep = b.dependency("googletest", .{});
+
     var flags = std.ArrayList([]const u8){};
     defer flags.deinit(b.allocator);
     try flags.appendSlice(b.allocator, &.{
@@ -41,4 +43,27 @@ pub fn build(b: *std.Build) !void {
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
+
+    const gtest_exe = b.addExecutable(.{
+        .name = "gtest",
+        .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
+    });
+    gtest_exe.root_module.addCSourceFiles(.{
+        // Test files go here.
+        .files = &.{
+            "src/test.cpp",
+        },
+        .flags = flags.items,
+    });
+    gtest_exe.linkLibrary(lib);
+    gtest_exe.linkLibrary(googletest_dep.artifact("gtest"));
+    gtest_exe.linkLibrary(googletest_dep.artifact("gtest_main"));
+    const gtest_step = b.step("gtest", "Run gtest");
+    const gtest_run = b.addRunArtifact(gtest_exe);
+    gtest_run.addArg("--gtest_brief=1");
+    gtest_step.dependOn(&gtest_run.step);
+    if (b.args) |args| gtest_run.addArgs(args);
+
+    const test_step = b.step("test", "Run all tests");
+    test_step.dependOn(&gtest_run.step);
 }
