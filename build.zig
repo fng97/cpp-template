6c3607cae8a387bd3fd24df6d9ea41f1e0cd377a
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-02-15 16:25:19 +0000

    Add benchmarking with Google benchmark

--- build.zig
+++ build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const googletest_dep = b.dependency("googletest", .{});
+    const benchmark_dep = b.dependency("benchmark", .{});
 
     var flags = std.ArrayList([]const u8){};
     defer flags.deinit(b.allocator);
@@ -15,12 +16,12 @@ pub fn build(b: *std.Build) !void {
         "-Wextra",
         "-Werror",
     });
+    if (optimize == .Debug) try flags.append(b.allocator, "-fstack-protector-all");
 
     const lib = b.addLibrary(.{
         .name = "lib",
         .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
     });
-    if (optimize == .Debug) try flags.append(b.allocator, "-fstack-protector-all");
     lib.root_module.addCSourceFiles(.{
         // Source files go here.
         .files = &.{
@@ -61,9 +62,31 @@ pub fn build(b: *std.Build) !void {
     const gtest_step = b.step("gtest", "Run gtest");
     const gtest_run = b.addRunArtifact(gtest_exe);
     gtest_run.addArg("--gtest_brief=1");
-    gtest_step.dependOn(&gtest_run.step);
     if (b.args) |args| gtest_run.addArgs(args);
+    gtest_step.dependOn(&gtest_run.step);
+
+    const gbench_exe = b.addExecutable(.{
+        .name = "gbench",
+        .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
+    });
+    gbench_exe.root_module.addCSourceFiles(.{
+        // Benchmark files go here.
+        .files = &.{
+            "src/benchmark.cpp",
+        },
+        .flags = flags.items,
+    });
+    gbench_exe.linkLibrary(lib);
+    gbench_exe.linkLibrary(benchmark_dep.artifact("benchmark"));
+    gbench_exe.linkLibrary(benchmark_dep.artifact("benchmark_main"));
+    const gbench_step = b.step("gbench", "Run gbench benchmarks");
+    const gbench_run = b.addRunArtifact(gbench_exe);
+    if (b.args) |args| gbench_run.addArgs(args);
+    gbench_step.dependOn(&gbench_run.step);
 
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&gtest_run.step);
+    const gbench_run_fast = b.addRunArtifact(gbench_exe);
+    gbench_run_fast.addArgs(&.{"--benchmark_min_time=0s"}); // only one iteration per benchmark
+    test_step.dependOn(&gbench_run_fast.step);
 }
